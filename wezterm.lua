c7a49a1fdb9891bb66b392192bb61755c4e63968
Kuro7s <kuro0970gamer@gmail.com>
2023-08-29 20:34:41 -0400

    idk

--- wezterm.lua
+++ wezterm.lua
@@ -8,9 +8,14 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Catppuccin Mocha'
+local rose_pine_theme = require 'rose-pine-theme.lua.rose-pine'
+
+config.colors = rose_pine_theme.colors()
+config.window_frame = rose_pine_theme.window_frame()
+
 config.default_prog = { 'nu' }
 config.font = wezterm.font 'CaskaydiaCove Nerd Font'
+config.font_size = 10
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 config.hide_mouse_cursor_when_typing = false
 config.hide_tab_bar_if_only_one_tab = true
@@ -19,31 +24,6 @@ config.window_padding = {
     right = 0,
     top = 0,
     bottom = 0,
-}
-config.window_close_confirmation = 'NeverPrompt'
-
-local current_colors = wezterm.color.get_builtin_schemes()[config.color_scheme]
-local curr_tab_colors = current_colors.tab_bar
-
-config.window_frame = {
-    inactive_titlebar_bg = curr_tab_colors.background,
-    active_titlebar_bg = curr_tab_colors.background,
-    inactive_titlebar_fg = curr_tab_colors.foreground,
-    active_titlebar_fg = curr_tab_colors.foreground,
-}
-
-config.colors = {
-    tab_bar = {
-		background = curr_tab_colors.background,
-        active_tab = {
-            bg_color = current_colors.background,
-            fg_color = current_colors.foreground,
-        },
-        inactive_tab = curr_tab_colors.inactive_tab,
-        inactive_tab_hover = curr_tab_colors.active_tab,
-        new_tab = curr_tab_colors.new_tab,
-        new_tab_hover = curr_tab_colors.new_tab_hover
-    }
 }
 
 wezterm.on('format-window-title', function()
